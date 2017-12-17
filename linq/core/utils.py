ffd6a3ee838f25727c053e4d6ccc9415ec7e193f
thautwarm <twshere@outlook.com>
2017-12-18 00:06:51 +0800

    to avoid wrong destructing params, now we just support the cases that we can ensure to destruct.

--- linq/core/utils.py
+++ linq/core/utils.py
@@ -9,16 +9,24 @@ def concat_generator(*generators):
 def is_single_param(f):
     if not callable(f):
         raise TypeError('Not callable argument!')
-    if not is_lambda(f):
-        return True
     try:
+        if getattr(f, '__code__', None) is None:
+            return True
+
         arg_info = inspect.getfullargspec(f)
     except TypeError:
         # In order to support some builtin functions :)
         return True
-    if arg_info.varargs:
-        return True
-    n = len(arg_info.args) + len(arg_info.kwonlyargs)
+
+    if not is_lambda(f):
+        if arg_info.varargs or arg_info.kwonlyargs or arg_info.defaults:
+            return True
+        n = len(arg_info.args)
+    else:
+        if arg_info.varargs:
+            return False
+        n = len(arg_info.args) + len(arg_info.kwonlyargs)
+
     if n is 0:
         raise ReferenceError('Function can not be with zero parameter.')
     return n is 1
